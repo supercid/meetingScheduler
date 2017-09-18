e4839c944904a21e73aa4f20256b4e4fb5338e15
Cid Lopes <alannettto@gmail.com>
2017-09-18 00:15:41 -0300

    Implements person CRUD views and Form Type
    Signed-off-by: Cid Lopes <alannettto@gmail.com>

--- src/AppBundle/Form/PersonType.php
+++ src/AppBundle/Form/PersonType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class PersonType extends AbstractType
 {
@@ -13,9 +14,15 @@ class PersonType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('socialSecurityNumber')->add('age')->add('gender');
+        $builder
+        ->add('name')
+        ->add('socialSecurityNumber')
+        ->add('age')
+        ->add('gender', ChoiceType::class, array(
+            'choices' => array('Male' => 'Male', 'Female' => 'Female'),
+        ));
     }
-    
+
     /**
      * {@inheritdoc}
      */
