cd53d1dc9c20fce8c084be7ee59591e9f151391f
Cid Lopes <alannettto@gmail.com>
2017-09-18 01:02:43 -0300

    Add script for easy run
    Signed-off-by: Cid Lopes <alannettto@gmail.com>

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+composer install
+php bin/console cache:clear
+php bin/console cache:clear --env=test
+php bin/console assets:install
+php bin/console doctrine:database:create
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:fixtures:load -q
+php bin/console doctrine:fixtures:load -q --env=test
+php phpunit.phar
+php bin/console server:run
